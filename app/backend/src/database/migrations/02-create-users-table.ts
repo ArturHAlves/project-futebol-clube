bba5096b4993c052f6d7d333d59357e7e87c98d7
Artur Henrique <arthenrdev@gmail.com>
2023-07-19 16:27:26 -0300

    feat: Req06 - Criação migrations, model - Users

---
+++ app/backend/src/database/migrations/02-create-users-table.ts
@@ -0,0 +1,31 @@
+import { Model, QueryInterface, DataTypes } from 'sequelize';
+import IUser from '../../Interfaces/IUser';
+
+export default {
+  up(queryInterface: QueryInterface) {
+    return queryInterface.createTable<Model<IUser>>('users', {
+      id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        primaryKey: true,
+        autoIncrement: true,
+      },
+      username: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      role: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      email: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      password: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+    });
+  },
+};
---
+++ app/backend/src/database/models/SequelizeUser.ts
@@ -0,0 +1,50 @@
+import {
+  CreationOptional,
+  DataTypes,
+  InferAttributes,
+  InferCreationAttributes,
+  Model,
+} from 'sequelize';
+
+import db from '.';
+
+export default class SequelizeUser extends Model<
+InferAttributes<SequelizeUser>,
+InferCreationAttributes<SequelizeUser>> {
+  declare id: CreationOptional<number>;
+  declare username: string;
+  declare role: string;
+  declare email: string;
+  declare password: string;
+}
+
+SequelizeUser.init({
+  id: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    primaryKey: true,
+    autoIncrement: true,
+  },
+  username: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  role: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  email: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  password: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+}, {
+  sequelize: db,
+  tableName: 'users',
+  modelName: 'User',
+  underscored: false,
+  timestamps: false,
+});
