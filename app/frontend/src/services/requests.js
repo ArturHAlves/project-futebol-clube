3fdf11238e21af764836d1889c4d0d722016db0f
Artur Henrique <arthenrdev@gmail.com>
2023-07-31 20:55:34 -0300

    feat: Criação variável Front-end docker

--- app/frontend/src/services/requests.js
+++ app/frontend/src/services/requests.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: `http://localhost:${process.env.REACT_APP_API_PORT || '3001'}`,
+  // baseURL: `http://localhost:${process.env.REACT_APP_API_PORT || '3001'}`,
+  baseURL: process.env.REACT_APP_APIURL,
 });
 
 export const setToken = (token) => {
